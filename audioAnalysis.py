14d247dba0c78603b32b91586d85c663fb08fb16
Wes <wmwilson87@gmail.com>
2024-04-15 09:12:00 -0700

    typo :)

--- audioAnalysis.py
+++ audioAnalysis.py
@@ -4,6 +4,6 @@ import librosa
 import numpy as np
 
 randA = np.random.randint(-1, high=1, size=2049).astype("float32")
-tempe, beats = librosa.beat.beat_track(y=randA, sr=22050)
+tempo, beats = librosa.beat.beat_track(y=randA, sr=22050)
 print("tempo: ", tempo)
 print("beats: ", beats)
