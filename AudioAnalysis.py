ea322a959b671a188048d3c64150d5bca188c6f1
Wes <wmwilson87@gmail.com>
2024-04-17 12:54:08 -0700

    Update and rename audioAnalysis.py to AudioAnalysis.py

---
+++ AudioAnalysis.py
@@ -0,0 +1,26 @@
+import librosa #v 0.10.1
+import sys
+import math
+import numpy as np
+
+def main(inF):
+    print("given in file:\n", inF)
+    #load into librosa and perform analysis
+    duration = 20
+    step = 5
+    offset = 0
+    totalD = math.floor(librosa.get_duration(path=inF))
+    print("total duration: ", totalD)
+    allY, allSr = librosa.load(inF)
+    allTempo, allBeats = librosa.beat.beat_track(y=allY, sr=allSr)
+    print("average tempo: ", allTempo)
+
+    while (offset < totalD):
+        y, sr = librosa.load(inF, offset=offset, duration=duration)
+        C = np.abs(librosa.cqt(y=y, sr=sr))
+        onset_env = librosa.onset.onset_strength(y=y, sr=sr, aggregate=np.median, fmax=8000, n_mels=256)
+        tempo, beats = librosa.beat.beat_track(onset_envelope=onset_env, sr=sr)
+        print("@ offset: ", offset, "tempo: ", tempo)
+        offset += step
+
+main(sys.argv[1])
--- audioAnalysis.py
+++
@@ -1,9 +0,0 @@
-print("Hello World!")
-
-import librosa
-import numpy as np
-
-randA = np.random.randint(-1, high=1, size=2049).astype("float32")
-tempo, beats = librosa.beat.beat_track(y=randA, sr=22050)
-print("tempo: ", tempo)
-print("beats: ", beats)
