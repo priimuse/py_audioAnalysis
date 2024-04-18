27d62beaa8029bbc57dc6244d853219f54079f1d
Wes <wmwilson87@gmail.com>
2024-04-17 20:46:29 -0700

    not passing in args at this time - the pathing is hardcoded as well for now

--- FileConversion.py
+++ FileConversion.py
@@ -1,14 +1,25 @@
 from moviepy.editor import * #v 1.0.3
 import sys
+import os
 
-def main(argsList):
-  inF = argsList[1]
-  extension = inF.split(".")[-1]
-  outF = inF.replace("." + extension, ".mp3")
-  vid = VideoFileClip(inF)
-  vid.audio.write_audiofile(outF)
-  if os.path.exists(outF): return outF
+def main():
+  inPath = os.path.join(os.getcwd(), "temp")
+  inF = ""
+  outF = ""
+  potentials = os.listdir(inPath)
+  for each in potentials:
+    each_split = each.split('.')
+    if each_split[0] == "out":
+      inF = os.path.join(inPath, "out." + each_split[1])
+      extension = each_split[1]
+      break
+
+  if inF != "":
+    outF = inF.replace("." + extension, ".mp3")
+    vid = VideoFileClip(inF)
+    vid.audio.write_audiofile(outF)
+  
+  if outF != "" and os.path.exists(outF): return outF
   else: return None
 
-ret = main(sys.argv)
-print("file converted to ", ret)
+print("file converted to ", main())
