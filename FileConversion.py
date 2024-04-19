b1bc808dcca9c3266409f9f4a8130dfc80f3ea1a
Wes <wmwilson87@gmail.com>
2024-04-18 18:21:05 -0700

    Update FileConversion.py

--- FileConversion.py
+++ FileConversion.py
@@ -2,24 +2,14 @@ from moviepy.editor import * #v 1.0.3
 import sys
 import os
 
-def main():
-  inPath = os.path.join(os.getcwd(), "temp")
-  inF = ""
-  outF = ""
-  potentials = os.listdir(inPath)
-  for each in potentials:
-    each_split = each.split('.')
-    if each_split[0] == "out":
-      inF = os.path.join(inPath, "out." + each_split[1])
-      extension = each_split[1]
-      break
-
-  if inF != "":
+def main(inF):
+  extension = inF.split('.')[-1]
+  if extension != "mp3":
     outF = inF.replace("." + extension, ".mp3")
     vid = VideoFileClip(inF)
     vid.audio.write_audiofile(outF)
-  
-  if outF != "" and os.path.exists(outF): return outF
-  else: return None
+    
+    if os.path.exists(outF): print(outF)
+  else: print(inF)
 
-print("file converted to ", main())
+main(sys.argv)
